66eef0c7bc3eb1db8978d7558e9c57aef907edd3
Sergio Guidon <guidons@outlook.com>
2023-03-30 15:05:31 +0200

    Update streamlit_app.py
    Added Snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import pandas
 import requests
 import snowflake.connector
 
+# Connect to Snowflake 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+
 # Import data and set index
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -33,3 +39,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Show normalized response
 streamlit.dataframe(fruityvice_normalized)
+
+# Test Snowflake connection
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
