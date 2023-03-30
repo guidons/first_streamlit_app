caf8a41df6263c7732e67f1566e0e3da6905d73c
Sergio Guidon <guidons@outlook.com>
2023-03-30 15:11:39 +0200

    Update streamlit_app.py
    Changed table display and fetched all results

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 # Import data and set index
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -41,5 +41,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # Test Snowflake connection
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
